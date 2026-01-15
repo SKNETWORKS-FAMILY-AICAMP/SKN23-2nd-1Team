3581e38a14f2ef4902ed6a685ceb19b6bee31b8b
clachic00 <anajrwk2@gmail.com>
2026-01-15 13:20:27 +0900

    프로젝트 수정

--- pages/insights.py
+++ pages/insights.py
@@ -20,6 +20,12 @@ with st.container(border=True):
     st.write("긍정적 감정 + 짧은 플레이 타임 → 이탈 신호가 가장 강하게 관측됨")
     st.write("부정적 감정 + 긴 플레이 타임 → 이탈 신호가 상대적으로 약하게 관측됨")
 
-with st.container(border=True):
-    col1, col2 = st.columns([5,5])
-    
+col1, col2 = st.columns([5,5])
+with col1:
+    with st.container(border=True):
+        st.image('images/insights/insight3.png')
+        st.write("플레이 집중도가 높을 수록 → 이탈 신호가 낮게 관측됨")
+with col2:
+    with st.container(border=True):
+        st.image('images/insights/insight4.png')
+        st.write("리뷰를 수정한 여부는 충성도의 지표 → 이탈 신호가 약하게 관측됨")
